bb817e6663679b7d16e25dc0c89a36a1319c5411
Arth Aggarwal <arth_aggarwal@s2022.ssts.edu.sg>
2025-02-10 11:44:27 +0800

    added the speech bubble stuffies to the menu and turn into function

--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
 # import functions and librarys here
+from text_on_image import text_on_image
 
 print("Welcome to the photobooth")
 print(
-    "Choose one to start with:\n [1] Sunglasses 1 \n [2] Sunglasses 2 \n [3] Sunglasses 3 \n [4] Sunglasses 4 \n [5] Sunglasses 5 \n Enter 0 to quit"
+    "Choose one to start with:\n [1] Sunglasses 1 \n [2] Sunglasses 2 \n [3] Sunglasses 3 \n [4] Sunglasses 4 \n [5] Sunglasses 5 \n [6] Write Text \n Enter 0 to quit"
 )
 props = []  # populate this with the image later
 
@@ -10,6 +11,9 @@ while True:
     option = int(input("Enter your option here: "))
     print("Loading ... ")
     # update with function later
+    if option == 6:
+        text = input("Enter the text you would like to show on the speech bubble: ")
+        text_on_image(text)
 
     if option == 0:
         break
--- text_on_image.py
+++ text_on_image.py
@@ -4,55 +4,55 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-img = Image.open("props/bubble.png")
-text = "They have shown that when Pt-WO3 film is ex- posed to H2 gas,They have shown that when Pt-WO3 film is ex- posed to H2 gas,They have shown that when Pt-WO3 film is ex- posed to H2 gas,"
-text_arr = text.split(" ")
 
-# variables for the text splitting
-formatted_text = ""
-current_line = ""
-max_chars_per_line = 30
+def text_on_image(text):
+    img = Image.open("props/bubble.png")
+    text_arr = text.split(" ")
 
+    # variables for the text splitting
+    formatted_text = ""
+    current_line = ""
+    max_chars_per_line = 30
 
-# Adding line breaks to the text so that it stays inside the speech bubble
-for word in text_arr:
-    if len(current_line) + len(word) + 1 > max_chars_per_line:
-        formatted_text += current_line.strip() + "\n"
-        current_line = word + " "
-    else:
-        current_line += word + " "
-# https://stackoverflow.com/questions/66721102/how-can-i-create-a-new-line-after-a-set-of-characters-in-python
+    # Adding line breaks to the text so that it stays inside the speech bubble
+    for word in text_arr:
+        if len(current_line) + len(word) + 1 > max_chars_per_line:
+            formatted_text += current_line.strip() + "\n"
+            current_line = word + " "
+        else:
+            current_line += word + " "
+    # https://stackoverflow.com/questions/66721102/how-can-i-create-a-new-line-after-a-set-of-characters-in-python
 
+    # Add the last line
+    formatted_text += current_line.strip()
 
-# Add the last line
-formatted_text += current_line.strip()
+    # variables for ensuring the lining part is correct
+    og_y = 1300
+    num_line_breaks = formatted_text.count("\n")
 
-# variables for ensuring the lining part is correct
-og_y = 1300
-num_line_breaks = formatted_text.count("\n")
+    # move the text accordingly
+    if num_line_breaks > 3:
+        for i in range(num_line_breaks - 3):
+            if og_y == 500:
+                break
+            og_y -= 400
 
-# move the text accordingly
-if num_line_breaks > 3:
-    for i in range(num_line_breaks - 3):
-        if og_y == 500:
-            break
-        og_y -= 400
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
 
-# Call draw Method to add 2D graphics in an image
-I1 = ImageDraw.Draw(img)
+    # Define the font and size
+    myFont = ImageFont.truetype("others/Prototype.ttf", 300)
 
-# Define the font and size
-myFont = ImageFont.truetype("others/Prototype.ttf", 300)
+    # Add Text to an image
+    I1.text(
+        (1900, og_y),
+        formatted_text,
+        font=myFont,
+        fill=(0, 0, 0),
+    )
 
-# Add Text to an image
-I1.text(
-    (1900, og_y),
-    formatted_text,
-    font=myFont,
-    fill=(0, 0, 0),
-)
+    # Display edited image
+    img.show()
 
-# Display edited image
-img.show()
-
-# Save the edited image
+    # Save the edited image
+    img.save("speech2.png")
