6041ea767e50cc85d38b1e4734b374fe2054810d
DeDizzyDoge <alexisachille2017@yandex.com>
2025-02-23 16:09:01 +0800

    added comments to camera.py and menu.py, added camera and debug choice for my sake, fixed the weird coding syntax

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+# libraries
 import cv2
 import numpy as np
 import mediapipe as mp
@@ -8,21 +9,20 @@ mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-
-def photobooth(chosen_prop, prop_type):
+def photobooth(chosen_prop, prop_type, camera, debug):
     # Open the webcam
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(camera)
 
     # Load the prop image with an alpha channel (transparency)
     image = cv2.imread(chosen_prop, cv2.IMREAD_UNCHANGED)
 
-    with mp_face_mesh.FaceMesh(
-        min_detection_confidence=0.5, min_tracking_confidence=0.5
-    ) as face_mesh:
+    # face detection
+    with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
         while cap.isOpened():
             ret, frame = cap.read()
 
-            start = time.time()
+            # Debug Mode - Start timer for FPS
+            if debug == 1: start = time.time()
 
             # Resize the frame to reduce processing time
             frame = cv2.resize(frame, (854, 480))
@@ -33,28 +33,27 @@ def photobooth(chosen_prop, prop_type):
             results = face_mesh.process(frame)
             frame.flags.writeable = True
 
+            # landmarks + prop placement
             if results.multi_face_landmarks:
                 for face_landmarks in results.multi_face_landmarks:
-                    mp_drawing.draw_landmarks(
-                        frame,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACEMESH_CONTOURS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec,
-                    )
+
+                    # Debug Mode - Display landmarks
+                    if debug == 1: mp_drawing.draw_landmarks(
+                            frame,
+                            landmark_list=face_landmarks,
+                            connections=mp_face_mesh.FACEMESH_CONTOURS,
+                            landmark_drawing_spec=drawing_spec,
+                            connection_drawing_spec=drawing_spec,)
 
                     # Convert face_landmarks to NumPy array for easier indexing
-                    landmarks = np.array(
-                        [
-                            (lm.x * frame.shape[1], lm.y * frame.shape[0])
-                            for lm in face_landmarks.landmark
-                        ]
-                    )
+                    landmarks = np.array([(lm.x * frame.shape[1], lm.y * frame.shape[0]) for lm in face_landmarks.landmark])
 
+                    # Initialise variables
                     left_eye, right_eye, nose_bridge = None, None, None
                     left_cheek, right_cheek = None, None
                     mouth_left, mouth_right, mouth_bottom = None, None, None
 
+                    # Index the specific landmarks for the prop
                     if prop_type == "glasses":
                         left_eye = landmarks[33]
                         right_eye = landmarks[263]
@@ -70,114 +69,35 @@ def photobooth(chosen_prop, prop_type):
                         mouth_right = landmarks[291]
                         mouth_bottom = landmarks[17]
 
-                    # Draw circles on specific landmarks if they are defined
-                    if (
-                        left_eye is not None
-                        and right_eye is not None
-                        and nose_bridge is not None
-                    ):
-                        cv2.circle(
-                            frame,
-                            (int(left_eye[0]), int(left_eye[1])),
-                            2,
-                            (0, 255, 0),
-                            -1,
-                        )
-                        cv2.circle(
-                            frame,
-                            (int(right_eye[0]), int(right_eye[1])),
-                            2,
-                            (0, 0, 255),
-                            -1,
-                        )
-                        cv2.circle(
-                            frame,
-                            (int(nose_bridge[0]), int(nose_bridge[1])),
-                            2,
-                            (255, 0, 0),
-                            -1,
-                        )
-
-                    if (
-                        prop_type == "masks"
-                        and left_cheek is not None
-                        and right_cheek is not None
-                    ):
-                        cv2.circle(
-                            frame,
-                            (int(left_cheek[0]), int(left_cheek[1])),
-                            2,
-                            (255, 255, 0),
-                            -1,
-                        )
-                        cv2.circle(
-                            frame,
-                            (int(right_cheek[0]), int(right_cheek[1])),
-                            2,
-                            (255, 0, 255),
-                            -1,
-                        )
-
-                    if (
-                        prop_type == "speech"
-                        and mouth_left is not None
-                        and mouth_right is not None
-                        and mouth_bottom is not None
-                    ):
-                        cv2.circle(
-                            frame,
-                            (int(mouth_left[0]), int(mouth_left[1])),
-                            2,
-                            (0, 255, 255),
-                            -1,
-                        )
-                        cv2.circle(
-                            frame,
-                            (int(mouth_right[0]), int(mouth_right[1])),
-                            2,
-                            (255, 255, 0),
-                            -1,
-                        )
-                        cv2.circle(
-                            frame,
-                            (int(mouth_bottom[0]), int(mouth_bottom[1])),
-                            2,
-                            (255, 0, 255),
-                            -1,
-                        )
+                    # Debug Mode - Draw circles on specific landmarks if they are defined
+                    if debug == 1:
+                        if (left_eye is not None and right_eye is not None and nose_bridge is not None):
+                            cv2.circle(frame, (int(left_eye[0]), int(left_eye[1])), 2, (0, 255, 0), -1,)
+                            cv2.circle(frame, (int(right_eye[0]), int(right_eye[1])), 2, (0, 0, 255), -1,)
+                            cv2.circle(frame, (int(nose_bridge[0]), int(nose_bridge[1])), 2, (255, 0, 0), -1,)
+                        if (prop_type == "masks" and left_cheek is not None and right_cheek is not None):
+                            cv2.circle(frame, (int(left_cheek[0]), int(left_cheek[1])), 2, (255, 255, 0), -1,)
+                            cv2.circle(frame, (int(right_cheek[0]), int(right_cheek[1])), 2, (255, 0, 255), -1,)
+                        if (prop_type == "speech" and mouth_left is not None and mouth_right is not None and mouth_bottom is not None):
+                            cv2.circle(frame, (int(mouth_left[0]), int(mouth_left[1])), 2, (0, 255, 255), -1, )
+                            cv2.circle(frame, (int(mouth_right[0]), int(mouth_right[1])), 2, (255, 255, 0), -1,)
+                            cv2.circle(frame, (int(mouth_bottom[0]), int(mouth_bottom[1])), 2, (255, 0, 255), -1,)
 
                     # Calculate position of the prop
-                    if (
-                        prop_type == "glasses"
-                        and left_eye is not None
-                        and right_eye is not None
-                    ):
+                    if (prop_type == "glasses" and left_eye is not None and right_eye is not None):
                         eye_distance = np.linalg.norm(left_eye - right_eye)
                         prop_width = int(eye_distance * 1.5)
-                        prop_height = int(
-                            prop_width * (image.shape[0] / image.shape[1])
-                        )
-                    elif (
-                        prop_type == "masks"
-                        and left_cheek is not None
-                        and right_cheek is not None
-                    ):
+                        prop_height = int(prop_width * (image.shape[0] / image.shape[1]))
+                    elif (prop_type == "masks" and left_cheek is not None and right_cheek is not None):
                         cheek_distance = np.linalg.norm(left_cheek - right_cheek)
                         prop_width = int(cheek_distance * 1.2)
-                        prop_height = int(
-                            prop_width * (image.shape[0] / image.shape[1])
-                        )
-                    elif (
-                        prop_type == "speech"
-                        and mouth_left is not None
-                        and mouth_right is not None
-                    ):
+                        prop_height = int(prop_width * (image.shape[0] / image.shape[1]))
+                    elif (prop_type == "speech" and mouth_left is not None and mouth_right is not None):
                         mouth_width = np.linalg.norm(mouth_left - mouth_right)
                         prop_width = int(mouth_width * 2)
-                        prop_height = int(
-                            prop_width * (image.shape[0] / image.shape[1])
-                        )
+                        prop_height = int(prop_width * (image.shape[0] / image.shape[1]))
 
+                    # Overlay the prop on the frame
                     if prop_type in ["glasses", "masks", "speech"]:
                         # resize prop
                         prop_resized = cv2.resize(image, (prop_width, prop_height))
@@ -187,11 +107,8 @@ def photobooth(chosen_prop, prop_type):
 
                         # Calculate the angle between left and right in radians
                         if prop_type == "glasses" or prop_type == "masks":
-                            angle_radians = -1 * np.arctan2(
-                                right_eye[1] - left_eye[1], right_eye[0] - left_eye[0]
-                            )
-                        elif prop_type == "speech":
-                            angle_radians = 0
+                            angle_radians = -1 * np.arctan2(right_eye[1] - left_eye[1], right_eye[0] - left_eye[0])
+                        elif prop_type == "speech": angle_radians = 0
 
                         # Convert to degrees
                         angle_degrees = np.degrees(angle_radians)
@@ -215,31 +132,21 @@ def photobooth(chosen_prop, prop_type):
                             x_offset = int(nose_bridge[0] - prop_width / 2)
                             y_offset = int(nose_bridge[1] - prop_height / 1.9)
                         elif prop_type == "speech":
-                            x_offset = int(mouth_right[0] - prop_width / 2 + 40)
-                            y_offset = int(mouth_bottom[1] - prop_height / 2 - 40)
+                            x_offset = int(mouth_right[0])
+                            y_offset = int(mouth_right[1] - prop_height)
 
                         # ensure overlay within proper bounds
-                        y1, y2 = max(0, y_offset), min(
-                            frame.shape[0], y_offset + prop_height
-                        )
-                        x1, x2 = max(0, x_offset), min(
-                            frame.shape[1], x_offset + prop_width
-                        )
+                        y1, y2 = max(0, y_offset), min(frame.shape[0], y_offset + prop_height)
+                        x1, x2 = max(0, x_offset), min(frame.shape[1], x_offset + prop_width)
 
                         # Adjust the overlay region if it goes out of bounds
                         overlay_y1 = max(0, -y_offset)
-                        overlay_y2 = prop_height - max(
-                            0, (y_offset + prop_height) - frame.shape[0]
-                        )
+                        overlay_y2 = prop_height - max(0, (y_offset + prop_height) - frame.shape[0])
                         overlay_x1 = max(0, -x_offset)
-                        overlay_x2 = prop_width - max(
-                            0, (x_offset + prop_width) - frame.shape[1]
-                        )
+                        overlay_x2 = prop_width - max( 0, (x_offset + prop_width) - frame.shape[1])
 
                         # Calculate the regions of interest
-                        overlay_roi = rotated_overlay[
-                            overlay_y1:overlay_y2, overlay_x1:overlay_x2
-                        ]
+                        overlay_roi = rotated_overlay[overlay_y1:overlay_y2, overlay_x1:overlay_x2]
                         frame_roi = frame[y1:y2, x1:x2]
 
                         # Ensure the shapes match before blending
@@ -252,26 +159,25 @@ def photobooth(chosen_prop, prop_type):
                             )
                             frame[y1:y2, x1:x2] = frame_roi
 
-            # display the frames per second
-            end = time.time()
-            fps = 1 / (end - start)
-            cv2.putText(
-                frame,
-                f"FPS: {fps:.2f}",
-                (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 255, 0),
-                2,
-                cv2.LINE_AA,
-            )
+            # Debug Mode - Display the FPS counter
+            if debug == 1:
+                end = time.time()
+                fps = 1 / (end - start)
+                cv2.putText(
+                    frame,
+                    f"FPS: {fps:.2f}",
+                    (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 255, 0),
+                    2,
+                    cv2.LINE_AA,)
 
             # Display the frame with the overlay
             cv2.imshow("Prop Filter", frame)
 
             # to break press q
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+            if cv2.waitKey(1) & 0xFF == ord("q"): break
 
     # close the camera
     cap.release()
--- menu.py
+++ menu.py
@@ -1,9 +1,22 @@
 # import functions and librarys here
 from text_on_image import text_on_image
 from camera import photobooth
-import cv2
 
-print("Welcome to the photobooth")
+print("Welcome to the Photobooth!")
+# camera for cv2.VideoCapture bc i have obs on my macbook sorry gang
+
+while True:
+    try:
+        camera = int(input("Which camera would you like to use? 0 for default, 1 for external: "))
+        if camera not in [0, 1]: raise ValueError
+        else: break
+    except ValueError:
+        print("Invalid input. Please input 0 or 1.") 
+# a debug mode that will show FPS counter and landmarks
+debug = input("Do you wish to turn on debug mode? 1 for Yes, any other input else for No: ") 
+if debug != "1": debug = 0
+else: debug = 1
+
 print(
     "Choose one to start with:\n [1] Black Sunglasses \n [2] Blue Sunglasses \n [3] Green Sunglasses \n [4] Red Sunglasses \n [5] Yellow Sunglasses \n [6] Write Text \n [7] Blue Mask \n [8] Red Mask \n [9] Yellow Mask \n Enter 10 to start the photobooth! \n Enter 0 to quit"
 )
@@ -20,24 +33,25 @@ props = [
     "props/yellow_mask.png",
 ]  # populate this with the image later
 chosen_type = ""
+chosen_prop = ""
 
 while True:
     option = int(input("Enter your option here: "))
     print("Loading ... ")
-    # update with function later
-    if option == 10:
+    if option == 10: # option to activate photobooth()
         if chosen_prop: break
         else: print("Please choose a prop first.")
-    if option == 6:
+    if option == 6: # speec bubble function
         text = input("Enter the text you would like to show on the speech bubble: ")
         text_on_image(text)
         chosen_prop = "props/speech2.png"
         chosen_type = "speech"
-        # path for the image is speech2.png, will create in the root of this folder
-    elif option in [1, 2, 3, 4, 5, 7, 8, 9]:
+    elif option in [1, 2, 3, 4, 5]: # glasses prop
         chosen_prop = props[option - 1]
         chosen_type = "glasses"
+    elif option in [7, 8, 9]: # glasses prop
+        chosen_prop = props[option - 1]
+        chosen_type = "masks"
     elif option == 0: break
 
-if option == 10:
-    photobooth(chosen_prop, chosen_type)
+if option == 10: photobooth(chosen_prop, chosen_type, camera, debug)
