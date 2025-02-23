9fc783e3be641e63f997397817a9171e2d704b1a
_an1mosity <99655540+aan1mosity@users.noreply.github.com>
2025-02-23 14:49:13 +0800

    updated menu.py to stop conflict

--- menu.py
+++ menu.py
@@ -1,12 +1,13 @@
-# import functions and libraries here
+# import functions and librarys here
 from text_on_image import text_on_image
-from camera import photobooth_glasses
+from camera import photobooth
 import cv2
 
 print("Welcome to the photobooth")
 print(
-    "Choose one to start with:\n [1] Black Sunglasses \n [2] Blue Sunglasses \n [3] Green Sunglasses \n [4] Red Sunglasses \n [5] Yellow Sunglasses \n [6] Write Text \n [7] Blue Mask \n [8] Red Mask \n [9] Yellow Mask \n Enter 10 to start the photobooth! \n Enter 0 to quit"
+    "Choose one to start with:\n [1] Sunglasses 1 \n [2] Sunglasses 2 \n [3] Sunglasses 3 \n [4] Sunglasses 4 \n [5] Sunglasses 5 \n [6] Write Text \n Enter 7 to stat the photobooth! \n Enter 0 to quit"
 )
+
 props = [
     "props/black_glasses.png",
     "props/blue_glasses.png",
@@ -18,17 +19,15 @@ props = [
     "props/red_mask.png",
     "props/yellow_mask.png",
 ]  # populate this with the image later
-chosen_prop = ""
+chosen_type = ""
 
 while True:
     option = int(input("Enter your option here: "))
     print("Loading ... ")
     # update with function later
     if option == 10:
-        if chosen_prop:
-            break
-        else:
-            print("Please choose a prop first.")
+        if chosen_prop: break
+        else: print("Please choose a prop first.")
     if option == 6:
         text = input("Enter the text you would like to show on the speech bubble: ")
         text_on_image(text)
@@ -36,8 +35,8 @@ while True:
         # path for the image is speech2.png, will create in the root of this folder
     elif option in [1, 2, 3, 4, 5, 7, 8, 9]:
         chosen_prop = props[option - 1]
-    elif option == 0:
-        break
+        chosen_type = "glasses"
+    elif option == 0: break
 
 if option == 10:
-    photobooth_glasses(chosen_prop)
+    photobooth(chosen_prop, chosen_type)
